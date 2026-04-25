0f3b56e80098044d263c89dff1702011925b7659
Mizuno <mizuno.webdesign@gmail.com>
2026-04-25 09:28:05 +0900

    feat: ファビコンをダーク背景Mに変更

---
+++ src/app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: "#1c241b",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span
+          style={{
+            color: "#ebe7da",
+            fontSize: "20px",
+            fontFamily: "serif",
+            fontWeight: 400,
+            letterSpacing: "-0.02em",
+            lineHeight: 1,
+          }}
+        >
+          M
+        </span>
+      </div>
+    ),
+    { ...size }
+  );
+}
